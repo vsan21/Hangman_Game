fd61d85b0c126c7c100ec12ed521d67bd8dea1b2
Vivian San <viviansan@Vivians-MacBook-Air.local>
2018-04-06 00:55:43 -0700

    Added feature to alert winner/loser

--- actions.js
+++ actions.js
@@ -6,12 +6,18 @@ let index;
 let word;
 let underScore;
 
-let remainingTries = 10;
+let remainingTries;
 
 $(document).ready(function() {
   //Creates a new game with new word.
   $('#new').click(function() {
     $('#hint').show();
+    $('#underscore').show();
+    $('#remaining').show();
+
+    //resets the remaining tries every time user presses new game
+    remainingTries = 10;
+    $('#remaining').html('Remaining Tries: ' + remainingTries)
     //Reassigns global variable, index --> generates a random number to be used for indexing
     index = Math.floor(Math.random() * wordList.length);
     //Reassigns global variable, word --> uses the randomly generated number to index to a specific word in the wordList array.
@@ -26,18 +32,29 @@ $(document).ready(function() {
   });
 
   $('.letters').click(function() {
+    // $('#remaining').show();
     for (let i = 0; i < word.length; i++) {
 
       //Compares the user's letter pick with every WORD index --> if match, then reassign the corresponding UNDERSCORE index with that letter (changes _ into a letter)
       if(word[i] === this.value) {
         underScore[i] = this.value;
         $('#underscore').html(underScore);
-      } else {
-        remainingTries -= 1;
-        $('#remaining').html('Remaining Tries: ' + remainingTries)
       }
     }
-
+    if(!underScore.includes("__ ")) {
+      alert('WINNER!');
+    }
+    //if the word DOES NOT include the button value, subtract 1 from remaining tries.
+    if(!word.includes(this.value)) {
+      remainingTries -= 1;
+      $('#remaining').html('Remaining Tries: ' + remainingTries)
+      //ending the game
+      if(remainingTries === 0) {
+        alert('SORRY GAME OVER');
+        $('#hint').hide();
+        $('#underscore').hide();
+      }
+    }
   });
 
 });
